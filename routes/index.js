f81564be207ba9e4532ee9492a74158759e4c67e
Thangesh <thangeswaran.kasirajan@wipro.com>
2018-08-28 08:06:26 +1000

    add sample json

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/prices', function(req, res, next) {
+  var json_info = [
+  	{A:[{year:2010,amt:124},{year:2011,amt:120}]},
+  	{B:[{year:2010,amt:10},{year:2011,amt:120}]},
+  	{C:[{year:2010,amt:133},{year:2012,amt:120}]},
+  	{D:[{year:2010,amt:120},{year:2013,amt:120}]},
+  ];
 
-	var json_info = [{name:'test'}]
-  res.status(500).json(json_info);
+ 
+
+  res.status(200).json(json_info);
 
 });
 
